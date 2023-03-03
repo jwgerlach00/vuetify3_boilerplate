6212ef95d980d7d56f328e4656c8a4c067d58f84
jwgerlach00 <jwgerlach00@gmail.com>
2023-03-03 11:36:33 -0500

    Set up axios and store and tested that axios calls worked (weird thing about credentials that will need to be verified on a Flask API

--- app/src/plugins/index.js
+++ app/src/plugins/index.js
@@ -8,10 +8,15 @@
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import router from '../router'
+import store from '../store'
+
+import axios from 'axios'
+// axios.defaults.withCredentials = true
 
 export function registerPlugins (app) {
   loadFonts()
   app
     .use(vuetify)
     .use(router)
+    .use(store)
 }
