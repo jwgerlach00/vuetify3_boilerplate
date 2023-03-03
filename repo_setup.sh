414d5ada08f47864c4a3397dcbb0058b30ead333
jwgerlach00 <jwgerlach00@gmail.com>
2023-03-03 13:16:19 -0500

    Changed repo_setup from named to positional arguments

--- repo_setup.sh
+++ repo_setup.sh
@@ -1,11 +1,10 @@
-while getopts n: flag
-do
-    case "${flag}" in
-        n) name=${OPTARG};;
-    esac
-done
+if [$1 == ""]
+then
+    echo "Enter a name of the GitHub repository as the argument."
+    exit 1
+fi
 
-git remote set-url origin https://github.com/jwgerlach00/$name
+git remote set-url origin https://github.com/jwgerlach00/$1
 git remote add upstream https://github.com/jwgerlach00/vuetify3_boilerplate
 git push origin master
 git push --all
