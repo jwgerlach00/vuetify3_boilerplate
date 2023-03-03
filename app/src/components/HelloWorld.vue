102d83e16b99af4f3b6615242cdc0ab2f2d9a7a0
jwgerlach00 <jwgerlach00@gmail.com>
2023-03-03 11:16:32 -0500

    Initialized using Vuetify3 documentation, HelloWorld modified and tested for v components

---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <v-text-field></v-text-field>
+    <v-select :items="items"></v-select>
+  </div>
+</template>
+
+<script setup>
+const items = ['foo', 'bar', 'baz']
+</script>
